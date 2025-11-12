cc7d9150951ac3940d74cbaaead2845e866477c1
Jenesh11 <pateljenesh079@gmail.com>
2025-11-12 13:10:58 +0530

    fix: format code and improve comments in sanity.cli.ts

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,15 +1,20 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'xs6ve8e6',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/cli#auto-updates
+     * Link this Studio to a specific Sanity app ID
+     * → Get your appId from https://www.sanity.io/manage/project/xs6ve8e6/studios
+     */
+    appId: 'B2h4toHQr',
+
+    /**
+     * Optional: keep auto updates on or turn off for version control
      */
     autoUpdates: true,
-  }
+  },
 })
